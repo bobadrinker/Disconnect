596b16fdd794ed658c13d2561a6b244e273be09f
Melissa <melissaappadoo.ma@gmail.com>
2022-03-15 11:21:52 +0000

    Set up Unity Project

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private Rigidbody2D myRigidbody;
+
+    private bool moving;
+
+    public float timeBetweenMove;
+    private float timeBetweenMoveCounter;
+    public float timeToMove;
+    private float timeToMoveCounter;
+
+    private Vector3 moveDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeBetweenMoveCounter = timeBetweenMove;
+        timeToMoveCounter = timeToMove;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
