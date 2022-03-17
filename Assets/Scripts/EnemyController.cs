2fc040e1e512f4e0290c35ad526a24a5516f638b
Melissa <melissaappadoo.ma@gmail.com>
2022-03-17 10:45:58 +0000

    Player Health System

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EnemyController : MonoBehaviour
 {
@@ -17,16 +18,73 @@ public class EnemyController : MonoBehaviour
 
     private Vector3 moveDirection;
 
+    public float waitToReload;
+    private bool reloading;
+    private GameObject thePlayer;
+    public string scene;
+
     // Start is called before the first frame update
     void Start()
     {
-        timeBetweenMoveCounter = timeBetweenMove;
-        timeToMoveCounter = timeToMove;
+        myRigidbody = GetComponent<Rigidbody2D>();
+
+        //timeBetweenMoveCounter = timeBetweenMove;
+        //timeToMoveCounter = timeToMove;
+
+        timeBetweenMoveCounter = Random.Range(timeBetweenMove * 0.75f, timeBetweenMove * 1.25f);
+        timeToMoveCounter = Random.Range(timeToMove * 0.75f, timeBetweenMove * 1.25f);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if (moving)
+        {
+            timeToMoveCounter -= Time.deltaTime;
+            myRigidbody.velocity = moveDirection;
+
+            if(timeToMoveCounter < 0f)
+            {
+                moving = false;
+                //timeBetweenMoveCounter = timeBetweenMove;
+                timeBetweenMoveCounter = Random.Range(timeBetweenMove * 0.75f, timeBetweenMove * 1.25f);
+            }
+
+        } else {
+            timeBetweenMoveCounter -= Time.deltaTime;
+            myRigidbody.velocity = Vector2.zero;
+
+            if(timeBetweenMoveCounter < 0f)
+            {
+                moving = true;
+                //timeToMoveCounter = timeToMove;
+                timeToMoveCounter = Random.Range(timeToMove * 0.75f, timeBetweenMove * 1.25f);
+
+                moveDirection = new Vector3(Random.Range (-1f, 1f) * moveSpeed, Random.Range(-1f, 1f) * moveSpeed, 0f);
+            }
+        }
+
+        if (reloading)
+        {
+            waitToReload -= Time.deltaTime;
+            if(waitToReload < 0)
+            {
+                SceneManager.LoadScene(scene);
+                thePlayer.SetActive(true);
+            }
+        }
+
+    }
+
+    void OnCollisionEnter2D (Collision2D other)
+    {
+        /*if(other.gameObject.name == "Player")
+        {
+            other.gameObject.SetActive(false);
+            reloading = true;
+            thePlayer = other.gameObject;
+        }*/
+
     }
 }
