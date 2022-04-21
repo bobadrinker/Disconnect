732087531ddfdd4c3309b3d235d12a4dfc820f9f
Melissa <melissaappadoo.ma@gmail.com>
2022-04-21 10:39:18 +0100

    miningame art added

---
+++ Assets/Scripts/DisableOnLoad.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DisableOnLoad : MonoBehaviour
+{
+    public string[] levels;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        foreach (string level in levels)
+        {
+            if (level == scene.name)
+            {
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                gameObject.SetActive(true);
+            }
+        }
+    }
+}
