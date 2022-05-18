f2a404f14b6d51171a9a8f85d7ac47c8972fc937
Melissa <melissaappadoo.ma@gmail.com>
2022-05-18 12:41:39 +0100

    minigame changes

---
+++ Assets/Scripts/MinigameManagaer.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinigameManagaer : MonoBehaviour
+{
+    public GameObject[] symbols;
+    public GameObject[] foods;
+    public GameObject bowl;
+
+    bool entered;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (foods.Length != 0)
+        {
+            int index = Random.Range(1, foods.Length);
+            symbols[index].SetActive(true);
+            if (entered)
+            {
+                symbols[index].SetActive(false);
+                Destroy(foods[index]);
+                entered = false;
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        entered = true;
+    }
+}
